fb618429bf2b6a4831aeb5324068b6de690688ef
MartinaSevero <marsevero88@gmail.com>
2021-05-15 17:43:14 -0300

    Se hacen arreglos en cuanto al diseño de la pantalla del form para ingresar datos

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
+import "date-fns";
+
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
-import "date-fns";
-import Grid from "@material-ui/core/Grid";
 import DateFnsUtils from "@date-io/date-fns";
 import {
   MuiPickersUtilsProvider,
   KeyboardTimePicker,
   KeyboardDatePicker,
 } from "@material-ui/pickers";
-import { Container } from "@material-ui/core";
-
+import { Container, Typography } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+//TODO arreglar para que sea responsive y los campos de fecha y hora se ponga uno abajo del otro
 const useStyles = makeStyles((theme) => {
-  console.log(theme);
   return {
     root: {
-      "& > *": {
+      "& .MuiTextField-root": {
         margin: theme.spacing(2),
+        width: "100%",
       },
     },
   };
@@ -25,19 +26,71 @@ const useStyles = makeStyles((theme) => {
 export function Form() {
   const classes = useStyles();
 
-  const [selectedDate, setSelectedDate] = useState(
-    new Date()
-  );
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
 
   return (
-    <form className={classes.root} noValidate autoComplete="off">
+    <form noValidate autoComplete="off">
+      <Grid container justify="center">
+        <Grid item className={classes.root}>
+          <Typography align="center" variant="h3" component="h2" gutterBottom>
+            Ingrese sus datos
+          </Typography>
+
+          <div>
+            <TextField id="standard-basic" label="Ingrese su nombre" />
+          </div>
+
+          <div>
+            <TextField id="standard-basic" label="Ingrese su CI" />
+          </div>
+        </Grid>
+      </Grid>
+
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <Grid container direction={"row"} justify="center" spacing={3}>
+          <Grid item>
+            <KeyboardDatePicker
+              margin="normal"
+              id="date-picker-dialog"
+              label="Selecione una fecha"
+              format="dd/MM/yyyy"
+              value={selectedDate}
+              onChange={handleDateChange}
+              KeyboardButtonProps={{
+                "aria-label": "change date",
+              }}
+            />
+          </Grid>
+
+          <Grid item>
+            <KeyboardTimePicker
+              margin="normal"
+              id="time-picker"
+              label="Seleccione una hora"
+              value={selectedDate}
+              onChange={handleDateChange}
+              KeyboardButtonProps={{
+                "aria-label": "change time",
+              }}
+            />
+          </Grid>
+        </Grid>
+      </MuiPickersUtilsProvider>
+    </form>
+  );
+}
+
+/*
+<form className={classes.root} noValidate autoComplete="off">
       <MuiPickersUtilsProvider utils={DateFnsUtils}>
         <Grid alignItems="center" container direction="column" justify="center">
           <Grid item xs="auto">
+            <h1>Ingrese sus datos</h1>
+
             <Container maxWidth="sm">
               <TextField id="standard-basic" label="Ingrese su nombre" />
             </Container>
@@ -76,5 +129,4 @@ export function Form() {
         </Grid>
       </MuiPickersUtilsProvider>
     </form>
-  );
-}
+*/
